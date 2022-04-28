159795b0717c172a689a9b134c7151195aef5eda
星冉 <troy@zygd.site>
2022-04-28 17:13:11 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # HTML-TO-MARKDOWN
-golang HTML-TO-MARKDOWN, html to markdown
+
+## Example
+
+```go
+import . "github.dev/XRSec/HTML-TO-MARKDOWN"
+
+body := GetSource(url, "", false)
+HtmlToMarkdown(body, output, true)
+```
+
+```bash
+go run awvs.go -u https://baidu.com -o 123.md
+```
+
+```bash
+htmltomd -u https://baidu.com -o 123.md
+```
+
+## Build
+
+```bash
+
+```
---
+++ htmltomd.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/JohannesKaufmann/html-to-markdown"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+var (
+	url                                      string
+	output                                   string
+	help                                     bool
+	version                                  bool
+	err                                      error
+	buildTime, commitId, versionData, author string
+)
+
+func init() {
+	log.SetPrefix("[HTML-TO-MARKDOWN] ")
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	flag.StringVar(&url, "url", "https://www.baidu.com", "The specified url")
+	flag.StringVar(&url, "u", "https://www.baidu.com", "The specified url")
+	flag.StringVar(&output, "output", "index.md", "File import Path")
+	flag.StringVar(&output, "o", "index.md", "File import Path")
+	flag.BoolVar(&help, "help", false, "Display help information")
+	flag.BoolVar(&help, "h", false, "Display help information")
+	flag.BoolVar(&version, "version", false, "HTML-TO-MARKDOWN version")
+	flag.BoolVar(&version, "v", false, "HTML-TO-MARKDOWN version")
+}
+
+func HtmlToMarkdown(html, output string, export bool) string {
+	var (
+		markdown string
+	)
+	converter := md.NewConverter("", true, nil)
+	if markdown, err = converter.ConvertString(html); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Successfully converted HTML to Markdown")
+	}
+	if export {
+		if err = ioutil.WriteFile(output, []byte(markdown), 0644); err != nil {
+			log.Fatal(err)
+		} else {
+			log.Printf("Successfully export Markdown to %v", output)
+		}
+	}
+	return markdown
+}
+
+func GetSource(url, output string, export bool) string {
+	var (
+		req  *http.Response
+		body []byte
+	)
+	http.TimeoutHandler(nil, time.Duration(10)*time.Second, "请求超时")
+	if req, err = http.Get(url); err != nil {
+		log.Fatalf("请求错误: Error: [%s]", err)
+	}
+	defer func(Body io.ReadCloser) {
+		if err = Body.Close(); err != nil {
+			log.Fatalf("关闭请求错误: Error: [%s]", err)
+		}
+	}(req.Body)
+	if body, err = ioutil.ReadAll(req.Body); err != nil {
+		log.Fatalf("读取错误: Error: [%s]", err)
+	}
+	if export {
+		if err = ioutil.WriteFile(output, body, 0644); err != nil {
+			log.Fatalf("写入错误: Error: [%s]", err)
+		} else {
+			log.Println("Successfully Download HTML!")
+		}
+	}
+	return string(body)
+}
+
+func main() {
+	flag.Parse()
+	if version {
+		fmt.Printf("Version: %v BuildTime:%v", versionData, buildTime)
+		fmt.Printf("Author: %v CommitId: %v", author, commitId)
+		return
+	}
+	if help || url == "" {
+		flag.Usage()
+		return
+	}
+
+	fmt.Printf("\n ╷───────────────────────────────────────────────── \n")
+	fmt.Printf(" │  HTML-TO-MARKDOWN								\n")
+	fmt.Printf(" │  URL: %v Output: %v					\n", url, output)
+	fmt.Printf(" ╵───────────────────────────────────────────────── \n\n")
+
+	body := GetSource(url, "", false)
+	HtmlToMarkdown(body, output, true)
+}
